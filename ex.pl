4aebf349ef3a16c8e8dc4d1ffeedaad305f1b87b
DR.IMAN <25507113+dr-iman@users.noreply.github.com>
2017-12-30 00:34:17 +0330

    Update ex.pl

--- ex.pl
+++ ex.pl
@@ -11,80 +11,56 @@ use HTTP::Request::Common qw(POST);
 use HTTP::Request::Common qw(GET);
 use URI::URL;
 use IO::Socket::INET;
-my $datetime    = localtime;
-$results="results";
-    if (-e $results) 
-    {
-    }
-    else
-    {
-        mkdir $results or die "can't creat dir: $results";
-    }
-	
-	system(($^O eq 'MSWin32') ? 'cls' : 'clear');
+
+system(($^O eq 'MSWin32') ? 'cls' : 'clear');
 	
 	
-$logo="
+print <<logo;                                                        
+ _____ _____ _____    ____      _           _           
+|     |     |   __|  |    \ ___| |_ ___ ___| |_ ___ ___ 
+|   --| | | |__   |  |  |  | -_|  _| -_|  _|  _| . |  _|
+|_____|_|_|_|_____|  |____/|___|_| |___|___|_| |___|_|  
+logo
 
 
-:'######::'##::::'##::'######::    ********************************                                                
-'##... ##: ###::'###:'##... ##:    * Coded By : DR-IMAN           *                                     
- ##:::..:: ####'####: ##:::..::    * Tel : DarkCod3r              *                                 
- ##::::::: ## ### ##:. ######::    * site : Guardiran.org         *                                       
- ##::::::: ##. #: ##::..... ##:    * Usage : perl cms.pl list.txt *                                               
- ##::: ##: ##:.:: ##:'##::: ##:    ********************************                                                
-. ######:: ##:::: ##:. ######::                                                    
-:......:::..:::::..:::......:::                                                    
-'########::'########:'########:'########::'######::'########::'#######::'########::
- ##.... ##: ##.....::... ##..:: ##.....::'##... ##:... ##..::'##.... ##: ##.... ##:
- ##:::: ##: ##:::::::::: ##:::: ##::::::: ##:::..::::: ##:::: ##:::: ##: ##:::: ##:
- ##:::: ##: ######:::::: ##:::: ######::: ##:::::::::: ##:::: ##:::: ##: ########::
- ##:::: ##: ##...::::::: ##:::: ##...:::: ##:::::::::: ##:::: ##:::: ##: ##.. ##:::
- ##:::: ##: ##:::::::::: ##:::: ##::::::: ##::: ##:::: ##:::: ##:::: ##: ##::. ##::
- ########:: ########:::: ##:::: ########:. ######::::: ##::::. #######:: ##:::. ##:
-........:::........:::::..:::::........:::......::::::..::::::.......:::..:::::..::
+print "\nPlease Enter Target List : ";
+my $list =<STDIN>;
+chomp($list);
+open (THETARGET, "<$list") || die "[-] Can't open the Targets List !";
+@TARGETS = <THETARGET>;
+close THETARGET;
+$link=$#TARGETS + 1;
+foreach $ma(@TARGETS){
 
-
-";
-
-print $logo;
-print "\t";
-
-
-open(tarrget,"<$ARGV[0]") or die "Please Insert Websites List\n";
-while(<tarrget>){
-chomp($_);
-$site = $_;
-if($site !~ /http:\/\//) { $site = "http://$site/"; };
+if($ma !~ /http:\/\//,/https:\/\//) { $ma = "http://$ma/"; };
 efrez();
-}
-sub efrez($site){
+
+sub efrez($ma){
 $ua = LWP::UserAgent->new(keep_alive => 1);
 $ua->agent("Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.31 (KHTML, like Gecko) Chrome/26.0.1410.63 Safari/537.31");
 $ua->timeout (10);
 
-my $efreez = $ua->get("$site")->content;
+my $efreez = $ua->get("$ma")->content;
 if($efreez =~/wp-content\/themes\/|wp-content\/plugins\/|wordpress/) {
-    print ("\n[WORDPRESS] $site\n");
+    print ("\n[WORDPRESS] $ma\n");
 open(save, '>>results/wp.txt');
-    print save "$site\n";
+    print save "$ma\n";
     close(save);
 }
 elsif($efreez =~/<script type=\"text\/javascript\" src=\"\/media\/system\/js\/mootools.js\"><\/script>|Joomla!|Joomla|joomla/) {
-    print ("\n[JOOMLA] $site\n",);
+    print ("\n[JOOMLA] $ma\n",);
 open(save, '>>results/jom.txt');
-    print save "$site\n";   
+    print save "$ma\n";   
     close(save);
     }
 elsif($efreez =~/\/modules\/system\/system.menus.css|\/sites\/default\/files\/|<meta name=\"Generator\" content=\"Drupal 7/) {
-    print ("\n[DRUPAL] $site\n",);
+    print ("\n[DRUPAL] $ma\n",);
 
 open(save, '>>results/Drupal.txt'); 
-    print save "$site\n";   
+    print save "$ma\n";   
     close(save);
     }else{
-    print "\n[UNKNOWN] $site\n";
+    print "\n[UNKNOWN] $ma\n";
 }
 }
-
-
+}
